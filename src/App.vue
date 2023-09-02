5c25856912b40fb40526a00387041ec88eadfc73
dreamik <dream-ik89@naver.com>
2023-09-02 17:05:54 +0900

    - function and variable
    변수와 함수 적용하기.

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
   </div>
   <!-- vue3 변경점 -->
   <p class="content">vue2에서는 root tag가 하나여야했지만 vue3에서는 루트태그가 하나가 아니어도 된다</p>
+  <div>
+    <h1>Call Function!</h1>
+    <p class="functionStrCall">{{greeting}}</p>
+    <p class="functionCall">{{greeting()}}</p>
+    <p class="functionCall">{{greeting("Father")}}</p>
+    <p>{{myAge("1888-01-01")}}</p>
+  </div>
+
 </template>
 
 <script>
@@ -14,9 +22,21 @@
 export default {
   setup(){//mount 할 때
     const name = 'Ik Cho';//변수를 지정하고
-
+    const greeting = (whom)=>{//함수 지정
+      if(!whom)whom=name;
+      return `Hi!!!! ${whom}`;
+    };
+    const myAge = (birthStr)=>{
+      const today = new Date();
+      const birthDay = new Date(birthStr);
+      const diff = today - birthDay;
+      const age = Math.floor(diff/(12*30*24*60*60*1000));
+      return `I'm ${age} years old.`
+    }
     return {
-      name//return에 obj 방식으로 이름을 입력
+      name,//return에 obj 방식으로 이름을 입력
+      greeting,//function도 export시킬 수 있음
+      myAge,
     }
   }
 }
@@ -33,5 +53,12 @@ div{
 }
 .content{
   color:#888888;
+}
+.functionStrCall{
+  text-decoration: line-through;
+  color:#FF0000FF;
+}
+.functionCall{
+  color: #42b983;
 }
 </style>
