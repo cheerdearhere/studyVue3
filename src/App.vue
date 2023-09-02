70761168b3289899ba9ee861b0808c2eb8d93c32
dreamik <dream-ik89@naver.com>
2023-09-02 21:29:33 +0900

    - dataBinding(단방향) : v-bind
    - 태그의 attribute를 동적으로 조절할 수 있음
    - v-on과 v-bind를 @와 :로 축약해 사용할 수 있다.

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,13 @@
     <div> Are you listen? {{afterClick}}</div>
 <!--  tag 내에서 ref(리터럴 적용)를 사용하는 경우에는 .value를 할 필요는 없음 -->
     <button
-        class="btn btn-success"
+        class="btn btn-outline-success btn-block"
         v-on:click="listeningFunction"
     >
       Click me!
     </button>
     <h1>Reactive</h1>
-    <button class="btn btn-warning" v-on:click="changeUserData">Change</button>
+    <button class="btn btn-outline-warning bin-block" v-on:click="changeUserData">Change</button>
     <div>
       <table class="table table-hover table-border">
         <thead>
@@ -45,76 +45,108 @@
       </table>
     </div>
   </div>
+  <div class="dataBind">
+    <h1>Data binding</h1>
+    <h5 class="label">Click: </h5>
+    <button class="btn btn-outline-success btn-sm" disabled>"Click me!"</button><br/>
+    <input v-bind:type="toggleType" v-bind:value="afterClick" v-bind:class="toggleClass"/>
+<!--
+  축약 가능
+    v-bind: => :
+    v-on: => @
+-->
+    <input type="text" :value="userData.name" @mouseover="changeUserData"/>
+  </div>
 </template>
 
 <script>
-//import 영역
-import { ref } from 'vue';//리터럴인 경우
-import {reactive} from "vue";//list, obj 등 자료형인 경우
-// js code
-export default {
-  setup(){//mount 할 때
-    const name = 'Ik Cho';//변수를 지정
-    let afterClick = ref("no...I'm waiting");
-    let toggleFlag =false;
-    const userData = reactive({
-      id:"1",
-      name:"김",
-      phone:"009",
-    })
-    const greeting = (whom)=>{//함수 지정
-      if(!whom)whom=name;
-      return `Hi!!!! ${whom}`;
-    };
-    const myAge = (birthStr)=>{
-      const today = new Date();
-      const birthDay = new Date(birthStr);
-      const diff = today - birthDay;
-      const age = Math.floor(diff/(12*30*24*60*60*1000));
-      return `I'm ${age} years old.`
-    }
-    const listeningFunction = ()=>{
-      toggleFlag = !toggleFlag;
-      //ref에 저정된 값을 변경시킴
-      afterClick.value = toggleFlag ? "Yes!" : "no...I'm waiting";//그냥 변수만 변경되고 화면은 변경안됨
-      //단 tag에서 쓰는 경우에는 .value를 안써도 됨
-    }
-    const changeUserData = ()=>{
-      toggleFlag = !toggleFlag;
-      userData.id     = toggleFlag ? "2" : "1";
-      userData.name   = toggleFlag ? "홍" : "김";
-      userData.phone  = toggleFlag ? "00100" : "009";
-    }
-    return {
-      name,//return에 obj 방식으로 이름을 입력
-      afterClick,
-      userData,
-      greeting,//function도 export시킬 수 있음
-      myAge,
-      listeningFunction,
-      changeUserData,
+  //import 영역
+  import { ref } from 'vue';//리터럴인 경우
+  import {reactive} from "vue";//list, obj 등 자료형인 경우
+  // codes
+  export default {
+    setup(){//mount 할 때
+      // variables
+      const name = 'Ik Cho';//변수를 지정
+      let afterClick = ref("no...I'm waiting");
+      let toggleFlag =false;
+      const userData = reactive({
+        id:"1",
+        name:"김",
+        phone:"009",
+      });
+      let toggleType = ref("text");
+      let toggleClass = ref("red");
+      // functions
+      const greeting = (whom)=>{//함수 지정
+        if(!whom)whom=name;
+        return `Hi!!!! ${whom}`;
+      };
+      const myAge = (birthStr)=>{
+        const today = new Date();
+        const birthDay = new Date(birthStr);
+        const diff = today - birthDay;
+        const age = Math.floor(diff/(12*30*24*60*60*1000));
+        return `I'm ${age} years old.`
+      }
+      const listeningFunction = ()=>{
+        toggleFlag = !toggleFlag;
+        //ref에 저정된 값을 변경시킴
+        afterClick.value = toggleFlag ? "Yes!" : "no...I'm waiting";//그냥 변수만 변경되고 화면은 변경안됨
+        //단 tag에서 쓰는 경우에는 .value를 안써도 됨
+        toggleType.value = toggleFlag ? "text" : "number";//type도 바인드 가능
+        toggleClass.value = toggleFlag ? "redName" : "blueName";
+      }
+      const changeUserData = ()=>{
+        toggleFlag = !toggleFlag;
+        userData.id     = toggleFlag ? "2" : "1";
+        userData.name   = toggleFlag ? "홍" : "김";
+        userData.phone  = toggleFlag ? "00100" : "009";
+      }
+      //export returns
+      return {
+        name,//return에 obj 방식으로 이름을 입력
+        afterClick,
+        userData,
+        toggleType,
+        toggleClass,
+
+        greeting,//function도 export시킬 수 있음
+        myAge,
+        listeningFunction,
+        changeUserData,
+
+      }
     }
   }
-}
 </script>
 
 <style>
-// stylesheet code
-div{
-  background: #fff;
-  color:#000;
-}
-.name{
-  font-weight: bolder;
-}
-.content{
-  color:#888888;
-}
-.functionStrCall{
-  text-decoration: line-through;
-  color:#FF0000FF;
-}
-.functionCall{
-  color: #42b983;
-}
+  // stylesheet code
+  div{
+    background: #fff;
+    color:#000;
+  }
+  .name{
+    font-weight: bolder;
+  }
+  .content{
+    color:#888888;
+  }
+  .functionStrCall{
+    text-decoration: line-through;
+    color:#FF0000FF;
+  }
+  .functionCall{
+    color: #42b983;
+  }
+  .label{
+    display:inline-block;
+  }
+  .redName {
+    color:red;
+  }
+  .blueName {
+    background-color:blue;
+  }
 </style>
