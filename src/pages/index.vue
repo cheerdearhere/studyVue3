06c314bc3d002df90f8e901e9cd574591912750a
조익 <dream-ik89@naver.com>
2023-11-27 13:12:42 +0900

    feat: toRefs를 사용해 reactive 속 객체 사용하기

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,25 @@
 <template>
   <h1>Home Page</h1>
+  <div>
+    <div>{{ count }}</div>
+    <button @click="count++">Add</button>
+  </div>
   <UseRef/>
 </template>
 
 <script>
 import UseRef from "@/components/UseRef.vue";
+import {useCount} from "@/composables/useCount";
+
 export default {
   components:{
     UseRef
+  },
+  setup(){
+    const {count} = useCount();
+    return {
+      count,
+    }
   }
 }
 </script>
